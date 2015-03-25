5bd0cece9effef9b9d651c6c85a8e37276f165d9
Diana Gergel <gergel@hydra.hydro.washington.edu>
2015-03-24 22:00:37 -0700

    fixed bug

--- qsub_qsub_gridcell_elevation.sh
+++ qsub_qsub_gridcell_elevation.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# basins="cascades california northernrockies southernrockies" 
+basins="cascades california northernrockies southernrockies" 
 # models="NorESM1-M CNRM-CM5 CSIRO-Mk3-6-0 CanESM2 MIROC5 bcc-csm1-1-m HadGEM2-CC365 HadGEM2-ES365 CCSM4 IPSL-CM5A-MR"
-# scenarios="historical rcp45 rcp85"
+scenarios="historical rcp45 rcp85"
 
-basins="california"
+#basins="california"
 # models="MIROC5"
-scenarios="historical"
+#scenarios="historical"
 
 ### loop over basins
 for basin in $basins
 do
-        for scenario in $scenario
+        for scenario in $scenarios
         do
-                echo "processing %basin %scenario" 
+                echo "processing $basin $scenario" 
 		qsub -v basin=$basin,scenario=$scenario qsub_grid_cell_elevation_mean.sh
         done
 done
