e2450964a3a1a0915a6b80eeeee25079a87f78f6
Diana Gergel <gergel@hydra.hydro.washington.edu>
2015-04-16 12:57:09 -0700

    fixed bug with masked arrays

--- calc_moist_deficit.py
+++ calc_moist_deficit.py
@@ -1,6 +1,9 @@
 #!/bin/python 
 
 import numpy as np
+import sys
+import os
+import math
 from snowpack_functions import lat_lon_adjust,unpack_netcdf_swe_month,mask_latlon,unpack_netcdf_file_var,historical_sum_swe
 ## get command line arguments 
 args = sys.argv[1:]
@@ -29,11 +32,11 @@ file_evap = '%s_ensavg_%s_%s.nc' %("Evaporation",scenario,basin)
 file_transp = '%s_ensavg_%s_%s.nc' %("Transp",scenario,basin)
 
 ## load data  
-lats,lons,petnat,datess_petnat = unpack_netcdf_file_var(direc,file_petnat,"petnat")
-lats,lons,petshort,datess_petshort = unpack_netcdf_file_var(direc,file_petshort,"petshort")
-lats,lons,pettall,datess_pettall = unpack_netcdf_file_var(direc,file_pettall,"pettall")
-lats,lons,evap,datess_evap = unpack_netcdf_file_var(direc,file_evap,"evap")
-lats,lons,transp,datess_transp = unpack_netcdf_file_var(direc,file_transp,"transp")
+lats,lons,petnat,datess_petnat = unpack_netcdf_file_var(direc,file_petnat,"PET_NatVeg")
+lats,lons,petshort,datess_petshort = unpack_netcdf_file_var(direc,file_petshort,"PET_Short")
+lats,lons,pettall,datess_pettall = unpack_netcdf_file_var(direc,file_pettall,"PET_Tall")
+lats,lons,evap,datess_evap = unpack_netcdf_file_var(direc,file_evap,"Evaporation")
+lats,lons,transp,datess_transp = unpack_netcdf_file_var(direc,file_transp,"Transp")
 
 ## adjust data for hydro years 
 petnat = petnat[9:-2,:,:]
@@ -41,28 +44,28 @@ petshort = petshort[9:-2,:,:]
 pettall = pettall[9:-2,:,:]
 evap = evap[9:-2,:,:]
 transp = transp[9:-2,:,:]
-for i in np.arange(len(swe)): ## loop over year
-	for j in np.arange(len(lats)): ## loop over latitude
-		for k in np.arange(len(lons)): ## loop over longitude
-			### don't calculate area for missing value elements
-                        if (math.isnan(swe[i,j,k])) == False:
-				if_in_box = mask_latlon(lats[j],lons[k],basin)
-				adjust_mask = lat_lon_adjust(lats[j],lons[k],basin)
-				mean_swe = historical_sum_swe(lats[j],lons[k]) 
-				## new historical swe function based on livneh instead of vic simulations 
-				if if_in_box and adjust_mask and mean_swe:
+for j in np.arange(len(lats)): ## loop over latitude
+	for k in np.arange(len(lons)): ## loop over longitude
+		### don't calculate area for missing value elements
+                if (math.isnan(swe[0,j,k])) == False:
+			if_in_box = mask_latlon(lats[j],lons[k],basin)
+			adjust_mask = lat_lon_adjust(lats[j],lons[k],basin)
+			mean_swe = historical_sum_swe(lats[j],lons[k]) 
+			## new historical swe function based on livneh instead of vic simulations 
+			if if_in_box and adjust_mask and mean_swe:
+				petsum = 0
+				aetsum = 0
+				for i in np.arange(len(swe)): ## now loop over year 
 					ind = i*12
-					pet_agg.append(np.sum(petnat[ind:ind+11,j,k]) + np.sum(petshort[ind:ind+11,j,k]) + np.sum(pettall[ind:ind+11,j,k]))
-					aet_agg.append(np.sum(evap[ind:ind+11,j,k]) + np.sum(transp[ind:ind+11,j,k])) 
-					lats_inc.append(lats[j])
-					lons_inc.append(lons[k])
+					petsum += np.sum(np.asarray(petnat[ind:ind+12,j,k])) + np.sum(np.asarray(petshort[ind:ind+12,j,k])) + np.sum(np.asarray(pettall[ind:ind+12,j,k]))
+					aetsum += np.sum(np.asarray(evap[ind:ind+12,j,k])) + np.sum(np.asarray(transp[ind:ind+12,j,k]))
+				pet_agg.append(petsum/len(swe))
+				aet_agg.append(aetsum/len(swe)) 
+				lats_inc.append(lats[j])
+				lons_inc.append(lons[k])
 
 ### save arrays to files 
 filearrayname = '/raid9/gergel/agg_snowpack/%s/moistdef_%s.npz' %(scenario,basin) 
-np.savez(filearrayname,lats=np.asarray(lats_inc),lons=np.asarray(lons_inc),pet=np.asarray(pet_agg),aet=np.asarray(aet))
+np.savez(filearrayname,lats=np.asarray(lats_inc),lons=np.asarray(lons_inc),pet=np.asarray(pet_agg),aet=np.asarray(aet_agg))
 print("finished analysis for %s %s" %(scenario,basin)) 			 
-
-
-
-
 
