7100c460bd42b43b803629a7c2c859a1d433154d
Diana Gergel <gergel@hydra.hydro.washington.edu>
2015-05-21 18:42:44 -0700

    made major changes, just elevation binning remains

--- plot_elev_vic_snotel_comp.py
+++ plot_elev_vic_snotel_comp.py
@@ -3,7 +3,7 @@
 import numpy as np
 import os
 import sys
-from snowpack_functions import lat_lon_adjust,mask_latlon 
+from snowpack_functions import lat_lon_adjust,mask_latlon,historical_sum_swe
 import glob
 from scipy import stats
 import datetime 
@@ -12,6 +12,8 @@ from vic_functions import get_snow_band,find_gridcell
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt 
+
+## function to get elevation for snotel site 
 def get_snotel_elevation(site_id):
 	snotel_file = '/raid9/gergel/vic_sim_obs/snotel_data/station.info'
 	snotel = np.loadtxt(snotel_file,dtype='str',delimiter = '\t') ## data is [ latitude longitude elevation snotel_id name_of_site] 
@@ -23,46 +25,53 @@ def get_snotel_elevation(site_id):
 			lon = line[1]
 	return(elev,lat,lon) 
 
-## step 1: get basin
+## step 1: get basin from input arguments 
 args = sys.argv[1:]
 basin = args[0]
 
 ## step 2: for each snotel site, extract elevation band from vic simulations closest to snotel elevation
+
 ## further mask out latlons that aren't part of the masks defined by lat_lon_adjust and mask_latlon 
 direc = '/raid9/gergel/agg_snowpack/snotel_vic/vic_output/%s' %basin 
 site_ids = list()
 for filename in os.listdir(direc): ## get list of snotel site ids 
 	site_ids.append(filename)
-if "11H59S" in site_ids: ## this is a missing snotel station in the Southern Rockies  
+if "11H59S" in site_ids: ## this is a missing snotel station in the Southern Rockies (i.e. it exists but isn't in Mu's drought monitoring dataset) 
 	site_ids.remove("11H59S")
-arr_site_ids = np.asarray(site_ids)
-vic_swe = list() 
-for site in arr_site_ids:
-	direcsite = '/raid9/gergel/agg_snowpack/snotel_vic/vic_output/%s/%s/fluxes__*' %(basin,site)
-	for pathfile in glob.glob(direcsite): 
-		path,fname = os.path.split(pathfile) 
-		elev,lat,lon = get_snotel_elevation(site)
-		snow_band,lat,lon = get_snow_band(fname,elev) ## get which snowband to use for snotel elevation
-		mask1 = lat_lon_adjust(float(lat),float(lon),basin)
-		mask2 = mask_latlon(float(lat),float(lon),basin)
-		if mask1 and mask2: ## apply further masking: include grid cell IF within mask 
-			if snow_band == 0:
-				data = np.loadtxt(pathfile,dtype='float',usecols=(3,),delimiter='\t')
-			elif snow_band == 1:
-				data = np.loadtxt(pathfile,dtype='float',usecols=(4,),delimiter='\t')
-			elif snow_band == 2: 
-				data = np.loadtxt(pathfile,dtype='float',usecols=(5,),delimiter='\t')
-			elif snow_band == 3:
-				data = np.loadtxt(pathfile,dtype='float',usecols=(6,),delimiter='\t')	
-			else: 
-				data = np.loadtxt(pathfile,dtype='float',usecols=(7,),delimiter='\t')
-			vic_swe.append(data[:]) ## add to vic swe list 
+arr_site_ids = np.asarray(site_ids) ## make list of site ids into array 
+vic_swe_elevs = list()
+vic_swe = list() ## array for holding full time series of simulated swe for each snotel (average over 4 surrounding grid cells) 
+for site in arr_site_ids: ## loop through snotel sites for mountain range 
+	vic_site_swe = list() 
+        direcsite = '/raid9/gergel/agg_snowpack/snotel_vic/vic_output/%s/%s/fluxes__*' %(basin,site)
+        for pathfile in glob.glob(direcsite): ## loop through 4 simulated grid cells around each vic snotel site 
+                path,fname = os.path.split(pathfile)
+                elev,lat,lon = get_snotel_elevation(site) ## get elevation, latitude and longitude of snotel site
+                snow_band,lat,lon = get_snow_band(fname,elev) ## get which snowband to use for snotel elevation
+                mask1 = lat_lon_adjust(float(lat),float(lon),basin) ## apply first lat/lon mask
+                mask2 = mask_latlon(float(lat),float(lon),basin) ## apply second lat/lon mask
+                # mask3 = historical_sum_swe(j,k) ## apply historical mean swe mask (using Livneh)
+                if mask1 and mask2: ## apply further masking: include grid cell IF within mask
+                        if snow_band == 0:
+                                data = np.loadtxt(pathfile,dtype='float',usecols=(3,),delimiter='\t')
+                        elif snow_band == 1:
+                                data = np.loadtxt(pathfile,dtype='float',usecols=(4,),delimiter='\t')
+                        elif snow_band == 2:
+                                data = np.loadtxt(pathfile,dtype='float',usecols=(5,),delimiter='\t')
+                        elif snow_band == 3:
+                                data = np.loadtxt(pathfile,dtype='float',usecols=(6,),delimiter='\t')
+                        else:
+                                data = np.loadtxt(pathfile,dtype='float',usecols=(7,),delimiter='\t')
+			vic_site_swe.append(data[:]) 
+	vic_swe.append(np.mean(np.asarray(vic_site_swe),axis=0)) ## append the average simulated swe for snotel site 
+	vic_swe_elevs.append(elev)  ## append elevation of snotel site 
 
-## step 3: average over all vic simulations 
-avg_vic = np.mean(np.asarray(vic_swe),axis=0)
-print(avg_vic.shape)
+## convert to array 
+vic_swe = np.asarray(vic_swe) ##[number of snotel stations,daily swe] 
+
 ## step 4: load snotel data, deal with missing values, average over all snotel data for the basin
 ## full array
+################ create full datetime array for indexing into vic and snotel swe arrays later on 
 base = datetime.datetime(1987, 1, 1)
 ## end date + 1 (will only produce specified end date - 1)
 end_date = datetime.datetime(2006, 1, 1)
@@ -76,10 +85,10 @@ for site in arr_site_ids:
 	snotel_dates = list()
 	print(site) 
 	filename = 'swe.%s.dat' %site
-	elev,lat,lon = get_snotel_elevation(site)
-	lat_sno,lon_sno = find_gridcell(float(lat),float(lon))
-	mask3 = lat_lon_adjust(float(lat_sno),float(lon_sno),basin)
-	mask4 = mask_latlon(float(lat_sno),float(lon_sno),basin)
+	elev,lat,lon = get_snotel_elevation(site) ## get elevation of snotel site 
+	lat_sno,lon_sno = find_gridcell(float(lat),float(lon)) ## figure out which gridcell the snotel site is in 
+	mask3 = lat_lon_adjust(float(lat_sno),float(lon_sno),basin) ## apply first lat/lon mask
+	mask4 = mask_latlon(float(lat_sno),float(lon_sno),basin) ## apply second lat/lon mask 
 	if mask3 and mask4: 
 		snotel_data = np.loadtxt(os.path.join(direc_snotel,filename),dtype='str',delimiter='\t')
 		for day in np.arange(len(snotel_data)):
@@ -102,31 +111,31 @@ for site in arr_site_ids:
 		#snotel_swe[rowcount,:] = a 
 		print(len(new_df['swe'].values))
 		rowcount += 1
-## calculate average of snotel swe 
-arr_snotel_swe = np.asarray(snotel_swe)
+
+## convert snotel list into array 
+arr_snotel_swe = np.asarray(snotel_swe) ## [number of snotel stations,daily swe] 
+arr_snotel_swe[arr_snotel_swe < 0] = np.nan ## if swe values are below zero, convert to nans 
 print(arr_snotel_swe.shape) 
-avg_snotel = stats.nanmean(arr_snotel_swe,axis=0)
-avg_snotel[avg_snotel < -100]=np.nan
-avg_snotel[avg_snotel < -5]=np.nan 
-print(avg_snotel.shape) 
-######################################################## step 5: plot snotel data and vic simulations ############################################################
-## plot all data
-plt.figure(figsize=(16,4))
-plt.plot(arr_dates,avg_vic,'b-',label='vic')
-plt.plot(arr_dates,avg_snotel,'r-',label='snotel')
-plt.legend() 
-plt.ylabel('SWE [mm]') 
-plt.title('SWE in %s' %basin) 
-plot_direc = '/raid9/gergel/agg_snowpack/snotel_vic/plots'
-plotname = '%s_all' %basin
-savepath = os.path.join(plot_direc,plotname)
-print("saving figure to '%s'" % savepath)
-plt.savefig(savepath) 
+
+## step 5: extract April 1 swe from vic and obs time series and average over the time period 
+april_dates = list()
+april_index = list() 
+for dayy in np.arange(len(arr_dates)):
+	if arr_dates[dayy].month == 4 and arr_dates[dayy].day == 1:
+		april_index.append(dayy) 
+		april_dates.append(arr_dates[dayy])
+april_index = np.asarray(april_index) ## this is an index array
+april_index_array = np.repeat(april_index.reshape(1,len(april_index)),len(vic_swe),axis=0) ## create index array for getting april values from vic and snotel swe 
+vicswe_april = np.take(vic_swe,april_index_array)
+snotelswe_april = np.take(arr_snotel_swe,april_index_array)
+## average over time period 
+vicswe_april_avg = np.mean(vicswe_april,axis=1)
+snotelswe_april_avg = np.mean(snotelswe_april,axis=1)  
+######################################################## step 6: plot snotel data and vic simulations ############################################################
 
 ## plot April 1 SWE 
 plt.figure(figsize=(16,4))
 ## get April 1 SWE from above vic and snotel arrays
-april_dates = list()
 april_vic = list()
 april_snotel = list()
 for dayy in np.arange(len(arr_dates)): 
@@ -144,52 +153,3 @@ savepath = os.path.join(plot_direc,plotname)
 print("saving figure to '%s'" %savepath)
 plt.savefig(savepath) 
 
-## plot maximum SWE every year (actual SWE and julian day) 
-year_range = np.arange(base.year,end_date.year,step=1)
-max_vic = list()
-max_snotel = list()
-max_snotel_dates = list()
-max_snotel_julian = list()
-max_vic_dates = list()
-max_vic_julian = list()
-for yearr in year_range: 
-	max_vic_list = list()
-	max_snotel_list = list()
-	max_date_list = list()
-	for dayyy in np.arange(len(avg_vic)):
-		if arr_dates[dayyy].year == yearr:
-			max_vic_list.append(avg_vic[dayyy])
-			max_snotel_list.append(avg_snotel[dayyy])
-			max_date_list.append(arr_dates[dayyy]) 
-	max_vic.append(np.nanmax(np.asarray(max_vic_list)))
-	max_snotel.append(np.nanmax(np.asarray(max_snotel_list)))
-	max_snotel_date = np.asarray(max_date_list)[np.nanargmin(np.asarray(max_vic_list))]
-	max_vic_date = np.asarray(max_date_list)[np.nanargmin(np.asarray(max_snotel_list))]
-	max_vic_dates.append(max_vic_date)
-	max_snotel_dates.append(max_snotel_date)
-	max_snotel_julian.append(max_snotel_date.timetuple().tm_yday) ## convert datetime to julian day
-	max_vic_julian.append(max_vic_date.timetuple().tm_yday) ## convert datetime to julian day 
-
-## plot
-plt.figure(figsize=(16,4))
-plt.plot(max_vic_dates,max_vic,'bs',markersize=7,label='vic')
-plt.plot(max_snotel_dates,max_snotel,'r*',markersize=7,label='snotel')
-plt.title('Maximum Yearly SWE in %s' %basin) 
-plt.ylabel('SWE [mm]') 
-plt.legend()
-plotname = '%s_maxswe' %basin 
-savepath = os.path.join(plot_direc,plotname)
-print("saving figure to '%s'" %savepath) 
-plt.savefig(savepath) 
-
-## plot
-plt.figure(figsize=(16,4))
-plt.plot(year_range.reshape(len(year_range),),max_vic_julian,'b-',label='vic')
-plt.plot(year_range.reshape(len(year_range),),max_snotel_julian,'r-',label='snotel')
-plt.title('Julian day of Maximum Yearly SWE in %s' %basin)
-plt.ylabel('Julian Day')
-plt.legend()
-plotname = '%s_julian_maxswe' %basin
-savepath = os.path.join(plot_direc,plotname)
-print("saving figure to '%s'" %savepath)
-plt.savefig(savepath) 
