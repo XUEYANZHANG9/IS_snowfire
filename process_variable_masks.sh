e31c21ffeaa91e9c39ebaf9cdb2fcae188a77d10
Diana Gergel <gergel@hydra.hydro.washington.edu>
2015-05-28 14:40:40 -0700

    added precip and soil moisture to variables included

--- process_variable_masks.sh
+++ process_variable_masks.sh
@@ -13,7 +13,7 @@ models="NorESM1-M CNRM-CM5 CSIRO-Mk3-6-0 CanESM2 MIROC5 bcc-csm1-1-m HadGEM2-CC3
 scenarios="historical rcp45 rcp85"
 basins="california cascades northernrockies southernrockies whites" 
 # variables="tasmax tasmin pr PET_NatVeg PET_Short PET_Tall Transp Evaporation"
-variables="SWE tasmax tasmin PET_NatVeg PET_Short PET_Tall Transp Evaporation"
+variables="SWE tasmax tasmin pr TotalSoilMoist PET_NatVeg PET_Short PET_Tall Transp Evaporation"
 # models="bcc-csm1-1-m CNRM-CM5 CSIRO-Mk3-6-0 CanESM2 CCSM4"
 
 for variable in $variables
