895d2b26e28360d979c0d6e016f548969ab6fd62
Diana Gergel <gergel@hydra.hydro.washington.edu>
2015-07-17 13:36:44 -0700

    fixed issue with using the wrong input files (were not masked as i thought)

--- clim_calc_ensavg.sh
+++ clim_calc_ensavg.sh
@@ -6,9 +6,12 @@
 #$ -q default.q
 #$ -N climatology
 
-variables="tasmax tasmin pr" 
+#variables="tasmax tasmin pr" 
+variables="pr tasmax tasmin"
 basin="california whites northernrockies southernrockies cascades"
+#basin="southernrockies"
 scenario="historical rcp45 rcp85"
+# scenario="rcp45 rcp85" 
 models="NorESM1-M CNRM-CM5 CSIRO-Mk3-6-0 CanESM2 MIROC5 bcc-csm1-1-m HadGEM2-CC365 HadGEM2-ES365 CCSM4 IPSL-CM5A-MR"
 arr_mod=($models)
 arr_bas=($basin)
@@ -16,18 +19,21 @@ arr_sc=($scenario)
 arr_vars=($variables)
 
 ## loop over models
-for var in 0 1 2  
+for var in 0 1 2   
 do
-	for bas in 0 1 2 3 4
+	for bas in 0 1 2 3 4 
 	do 
-		for scen in 0 1 2 
+		for scen in 0 1 2
+		#for scen in 0 1 
 		do
 			#### and which scenario it is
 			if [ "historical" = "${arr_sc[$scen]}" ]
 			then
 			years="1970-1999"
+			yearss="1970_1999"
 			else
 			years="2010-2039"
+			yearss="2010_2039"
 			fi
 
 			for i in 0 1 2 3 4 5 6 7 8 9
@@ -36,7 +42,7 @@ do
 				then
 				eval "ifile${i}="/raid9/gergel/agg_snowpack/goodleap/${arr_bas[$bas]}/${arr_mod[$i]}__${arr_sc[$scen]}.${timecalc}.${arr_vars[$var]}.${years}_${arr_bas[$bas]}_${sm_month}.nc""
 				else
-				eval "ifile${i}="/raid9/gergel/agg_snowpack/gcms_var/${arr_vars[$var]}_${arr_bas[$bas]}_${arr_mod[$i]}__${arr_sc[$scen]}.${years}.nc""
+				eval "ifile${i}="/raid9/gergel/agg_snowpack/goodleap/${arr_bas[$bas]}/${arr_mod[$i]}__${arr_sc[$scen]}.${timecalc}.${arr_vars[$var]}.${yearss}_${arr_bas[$bas]}.nc""
 				fi
 			done
 			if [ "TotalSoilMoist" = "${arr_vars[$var]}" ]
