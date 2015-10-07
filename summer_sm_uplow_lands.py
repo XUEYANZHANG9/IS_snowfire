d1f7fdeef4eb489138719833b7d0c7669615e29f
Diana Gergel <gergel@hydra.hydro.washington.edu>
2015-10-07 16:28:04 -0700

    updated to include precip and temp inputs but update not yet complete

--- summer_sm_uplow_lands.py
+++ summer_sm_uplow_lands.py
@@ -37,7 +37,7 @@ if (type == "ensavg"):
 else: 
 	model = args[3]
 	scenario = args[4] 
-if (variable == "pr"):
+if (variable == "pr") or (variable == "tasmin") or (variable == "tasmax"):
 	season = args[5]
 if (scenario == "historical"):
 	years = "1950_2005"
@@ -60,8 +60,8 @@ if (variable == "TotalSoilMoist"):
 	filename_hist = '%s__%s.monmean.TotalSoilMoist.%s_%s_summer.nc' % (model,"historical","1950_2005",basin)
 	filename = '%s__%s.monmean.TotalSoilMoist.%s_%s_summer.nc' % (model,scenario,years,basin)
 else:
-	filename_hist = '%s__%s.monsum.pr.%s_%s_%s.nc' % (model,"historical","1950_2005",basin,season)
-        filename = '%s__%s.monsum.pr.%s_%s_%s.nc' % (model,scenario,years,basin,season)
+	filename_hist = '%s__%s.monsum.%s.%s_%s_%s.nc' % (model,"historical",variable,"1950_2005",basin,season)
+        filename = '%s__%s.monsum.%s.%s_%s_%s.nc' % (model,scenario,variable,years,basin,season)
 
 ## load data
 if (variable == "TotalSoilMoist"):
@@ -70,8 +70,8 @@ if (variable == "TotalSoilMoist"):
 else:
 	print(os.path.join(direc,filename_hist))
 	print(os.path.join(direc,filename))
-	lats,lons,sm_hist,datess_hist = unpack_netcdf_file_var(direc,filename_hist,"pr")
-        lats,lons,sm,datess = unpack_netcdf_file_var(direc,filename,"pr")
+	lats,lons,sm_hist,datess_hist = unpack_netcdf_file_var(direc,filename_hist,variable)
+        lats,lons,sm,datess = unpack_netcdf_file_var(direc,filename,variable)
 
 ## load historical mean SWE file and get mask
 direc = '/raid9/gergel/agg_snowpack/goodleap/SWE'
@@ -85,6 +85,12 @@ lats_swe,lons_swe,hist_noswe,datess_swe = unpack_netcdf_file_var(direc,filename_
 ## masking for lowlands 
 if (basin == "nwinterior") or (basin == "missouri") or (basin == "coastalnorth") or (basin == "coastalsouth") or (basin == "lower_colorado") or (basin == "great_basin"):
 	
+	## number of months varies depending on winter vs. summer
+	if (season == "winter"):
+		mos = 5
+	else:
+		mos = 3
+
 	if (basin == "coastalnorth"):
 		sm_swe_mask = np.ma.logical_and(hist_noswe,sm) ## mask with no swe mask  
 		sm_swe_mask_hist = np.ma.logical_and(hist_noswe,sm_hist) ## mask with no swe mask 
@@ -94,16 +100,20 @@ if (basin == "nwinterior") or (basin == "missouri") or (basin == "coastalnorth")
 	
 		a,b,c = sm_masked.shape
 	
-		sm_masked_res = sm_masked.reshape(3,a/3,b,c) 
-		sm_hist_masked_res = sm_hist_masked.reshape(3,56,b,c) 
+		sm_masked_res = sm_masked.reshape(mos,a/mos,b,c) 
+		sm_hist_masked_res = sm_hist_masked.reshape(mos,56,b,c) 
 	
 	else: 
 		a,b,c = sm.shape
-		sm_masked_res = sm.reshape(3,a/3,b,c)
-		sm_hist_masked_res = sm_hist.reshape(3,56,b,c) 
+		sm_masked_res = sm.reshape(mos,a/mos,b,c)
+		sm_hist_masked_res = sm_hist.reshape(mos,56,b,c) 
 
-	sm_hist_f = sm_hist_masked_res.mean(0) 
-	sm_f = sm_masked_res.mean(0)
+	if (variable == "pr"):
+		sm_hist_f = sm_hist_masked_res.sum(0) 
+        	sm_f = sm_masked_res.sum(0)
+	else:
+		sm_hist_f = sm_hist_masked_res.mean(0) 
+		sm_f = sm_masked_res.mean(0)
 	
 	if (basin == "coastalnorth"):
 		sm_hist_final = sm_hist_f
@@ -174,12 +184,15 @@ else:
 
 	a,b,c = sm_masked_full.shape
 	
-	sm_masked_res = sm_masked_full.reshape(3,a/3,b,c) 
-	sm_hist_masked_res = sm_hist_masked_full.reshape(3,56,b,c)
+	sm_masked_res = sm_masked_full.reshape(mos,a/mos,b,c) 
+	sm_hist_masked_res = sm_hist_masked_full.reshape(mos,56,b,c)
 	
-	
-	sm_hist_final = sm_hist_masked_res.mean(0) 
-        sm_final = sm_masked_res.mean(0) 
+	if (variable == "pr"):
+		sm_hist_final = sm_hist_masked_res.sum(0) 
+        	sm_final = sm_masked_res.sum(0)
+	else: 
+		sm_hist_final = sm_hist_masked_res.mean(0) 
+        	sm_final = sm_masked_res.mean(0) 
 
 	
 
@@ -200,16 +213,34 @@ sm_minstor_area = np.ma.multiply(sm_in_storage,cellareas)*0.000001 ## also conve
 #sm_sum = sm_minstor_area.sum(axis=(1,2)) 
 sm_sum = sm_in_storage.sum(axis=(1,2))*0.000001
 sm_full = sm_in_storage*0.000001
+
+## for precip and temp analysis
+if (scenario == "historical"):
+	if (basin == "missouri") or (basin == "lower_colorado") or (basin == "great_basin") or (basin == "coastalnorth") or (basin == "coastalsouth") or (basin == "nwinterior"):
+		var = sm_final[19:-7,:,:].mean(0).compressed()
+	else:
+		maskk = 
+else:
+	var1 = sm_final[3:33,:,:].mean(0).compressed()
+	var2 = sm_final[33:63,:,:].mean(0).compressed()
+	var3 = sm_final[63:93,:,:].mean(0).compressed()
 ##### save arrays to files
 if (type == "ensavg"):
 	filearrayname = '/raid9/gergel/agg_snowpack/sm_summer/%s_%s.npz' %(basin,scenario)
 else:
-	if (variable == "pr"):
-		filearrayname = '/raid9/gergel/agg_snowpack/precip_summer/%s_%s_%s_%s.npz' %(basin,model,scenario,season)
+	if (variable == "pr") or (variable == "tasmin") or (variable == "tasmax"):
+		filearrayname = '/raid9/gergel/agg_snowpack/climatology/arrays/%s_%s_%s_%s_%s.npz' %(variable,basin,model,scenario,season)
 	else:
 		filearrayname = '/raid9/gergel/agg_snowpack/sm_summer/%s_%s_%s.npz' %(basin,model,scenario)
 
-np.savez(filearrayname,sm=np.asarray(sm_sum),sm_f=np.asarray(sm_full))
+if (variable == "TotalSoilMoist"):
+	np.savez(filearrayname,sm=np.asarray(sm_sum),sm_f=np.asarray(sm_full))
+else:
+	if (scenario == "historical"):
+		np.savez(filearrayname,var=var)
+	else:
+		np.savez(filearrayname,chunk1=var1,chunk2=var2,chunk3=var3)
+
 
 if (type == "ensavg"):
 	print("finished running script successfully for %s %s" %(basin,scenario))  
