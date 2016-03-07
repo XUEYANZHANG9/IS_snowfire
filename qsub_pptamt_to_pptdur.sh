716e7212eae2613567625ec8ced70af82449aa38
Diana Gergel <gergel@hydra.hydro.washington.edu>
2016-03-07 13:54:03 -0800

    updated qsub elevations file

--- qsub_pptamt_to_pptdur.sh
+++ qsub_pptamt_to_pptdur.sh
@@ -4,10 +4,13 @@
 #$ -j y
 #$ -S /bin/bash
 #$ -q default.q
-#$ -N pptdur
+#$ -N elevs
 
 source /home/gergel/.bashrc
 source activate /home/gergel/anaconda/envs/my_env
 
-python /raid9/gergel/agg_snowpack/pptamt_to_pptdur.py $model $scenario
+# python /raid9/gergel/agg_snowpack/pptamt_to_pptdur.py $model $scenario
 
+python /raid9/gergel/agg_snowpack/elevations_for_dfm.py
+
+#python /raid9/gergel/agg_snowpack/slice_dfm_inputs.py $variable $model $scenario  
