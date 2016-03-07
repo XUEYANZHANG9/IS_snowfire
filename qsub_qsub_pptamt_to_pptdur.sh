2baf1fa8c91d3198c7c0a553a0700632b8cd49c1
Diana Gergel <gergel@hydra.hydro.washington.edu>
2016-03-07 13:56:17 -0800

    get this branch up to date

--- qsub_qsub_pptamt_to_pptdur.sh
+++ qsub_qsub_pptamt_to_pptdur.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-basins="cascades california northernrockies southernrockies whites" 
 models="NorESM1-M CNRM-CM5 CSIRO-Mk3-6-0 CanESM2 MIROC5 bcc-csm1-1-m HadGEM2-CC365 HadGEM2-ES365 CCSM4 IPSL-CM5A-MR"
 scenarios="historical rcp45 rcp85"
+#variables="tmin tmax rh pptdur" 
 
-# basins="whites"
 #models="CNRM-CM5"
 #scenarios="historical"
+variables="pptdur"
 
 ### loop over basins
-for model in $models
-do
-	for scenario in $scenarios
-	do 
-		echo "processing $model $scenario"
-        	qsub -v model=$model,scenario=$scenario qsub_pptamt_to_pptdur.sh 
-	done  
+for variable in $variables
+do 
+	for model in $models
+	do
+		for scenario in $scenarios
+		do 
+			echo "processing $variable $model $scenario"
+        		qsub -v variable=$variable,model=$model,scenario=$scenario qsub_pptamt_to_pptdur.sh 
+			#python /raid9/gergel/agg_snowpack/slice_dfm_inputs.py $variable $model $scenario
+		done  
+	done 
 done
 
