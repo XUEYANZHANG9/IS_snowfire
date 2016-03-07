edd4ab72058d7abe45025142eb7cf9f7af6a5156
Diana Gergel <gergel@hydra.hydro.washington.edu>
2016-03-06 21:37:42 -0800

    regridded beta paramater using cdo remapcon and fixed latitude issue with xray.align

--- pptamt_to_pptdur.py
+++ pptamt_to_pptdur.py
@@ -1,19 +1,14 @@
-
-# coding: utf-8
-
-# In[3]:
-
 import xray 
 import numpy as np
 import sys
 import os
 import gc 
-
-# In[6]:
+from xarray import ufuncs 
 
 args = sys.argv[1:]
 model = args[0]
 scenario = args[1]
+
 #model = "CNRM-CM5"
 #scenario = "historical"
 
@@ -23,7 +18,10 @@ pr = xray.open_dataset(os.path.join(direc,pr_file)) ## load precip
 
 ## adjust lat/lon dimensions since the index names are different
 lons_new = pr['lon'].values[pr['lon'].values > 180] - 360 
-pr['lon'] = lons_new
+
+# address rounding error issue with floating point differences in xray.align 
+pr['lon'] = np.round(lons_new, 5) 
+pr['lat'] = np.round(pr['lat'], 5) 
 
 swe_mask_file = '/raid9/gergel/agg_snowpack/goodleap/SWE/histmeanmask.nc' ## 1s are swe, 0s are no swe 
 swe_mask = xray.open_dataset(swe_mask_file)
@@ -35,19 +33,15 @@ swe_mask = swe_mask.squeeze()
 
 ## Dataset join 
 swe_mask_align,pr_full = xray.align(swe_mask,pr,join='inner',copy=False)
-#swe_mask_align,tmax_align = xray.align(swe_mask,tmax,join='inner',copy=False)
 
 del pr 
 gc.collect() 
 
 direc = '/raid/gergel'
-pdur_file = 'pduration.nc'
+pdur_file = 'pduration_mod.nc' # this is the one that I regridded to 6 x 6 km using cdo remapcon and /raid9/gergel/agg_snowpack/keepextracopies/grid_info_mine
 pdur_full = xray.open_dataset(os.path.join(direc,pdur_file)) ## pdur beta parameter for John's transform from Matt Jolly
 
-
-# In[13]:
-
-# pr_arr = pr_full['precipitation'].values
+'''
 for j in np.arange(len(pr_full.lat)):
     for k in np.arange(len(pr_full.lon)):
         if np.isnan(pr_full['precipitation'].values[0,j,k]) == False: 
@@ -57,9 +51,10 @@ for j in np.arange(len(pr_full.lat)):
             #u = pr_arr[:,j,k]
             # u = np.round(24 * (1 - (np.exp(-beta*pr_full['precipitation'].values[:,j,k]))))
 	    pr_full['precipitation'].values[:,j,k] = np.round(24 * (1 - (np.exp(-beta*pr_full['precipitation'].values[:,j,k]))))
-# In[ ]:
+'''
 
-#pr_full['precipitation'].values = pr_arr
+pr_full['precipitation'].values = np.round(24 * (1 - (ufuncs.exp(-pdur_full['pdur'].values * pr_full['precipitation'].values))))
+
 direc = '/raid/gergel/pptdur'
 filename = '%s_%s.nc' % (model,scenario)
 pr_full.to_netcdf(os.path.join(direc,filename))
