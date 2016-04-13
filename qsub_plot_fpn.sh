0f3259ce7e1413a6134a8c73c80cbbda19d0b56e
Diana Gergel <gergel@hydra.hydro.washington.edu>
2016-04-13 13:18:15 -0700

    fixed plotting issue

---
+++ qsub_plot_fpn.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#$ -q default.q
+#$ -N fpn_100
+
+source /home/gergel/.bashrc
+source activate /home/gergel/anaconda/envs/my_env
+
+#python plot_summer_sm.py 
+
+# python plot_1000_fpn.py 
+
+python plot_100_fpn_rev.py 
