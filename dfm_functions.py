b3abe92e132a15e89f1044ab3b01d0b27916cb61
Diana Gergel <gergel@hydra.hydro.washington.edu>
2016-04-15 10:22:39 -0700

    added discrete colorbar function

--- dfm_functions.py
+++ dfm_functions.py
@@ -80,3 +80,23 @@ def make_map(fs, label_parallels=None, label_meridians=None):
     m.drawlsmask(land_color='silver',ocean_color='lightblue',lakes=True,resolution='c')
     return(m)
 
+def cmap_discretize(cmap, N):
+    """Return a discrete colormap from the continuous colormap cmap.
+    cmap: colormap instance, eg. cm.jet. 
+    N: number of colors.
+      
+    Example
+    x = resize(arange(100), (5,100))
+    djet = cmap_discretize(cm.jet, 5)
+    imshow(x, cmap=djet)
+    """
+    if type(cmap) == str:
+        cmap = get_cmap(cmap)
+    colors_i = concatenate((linspace(0, 1., N), (0.,0.,0.,0.)))
+    colors_rgba = cmap(colors_i)
+    indices = linspace(0, 1., N+1)
+    cdict = {}
+    for ki,key in enumerate(('red','green','blue')):
+        cdict[key] = [ (indices[i], colors_rgba[i-1,ki], colors_rgba[i,ki]) for i in xrange(N+1) ]
+    # Return colormap object.
+    return matplotlib.colors.LinearSegmentedColormap(cmap.name + "_%d"%N, cdict, 1024)
